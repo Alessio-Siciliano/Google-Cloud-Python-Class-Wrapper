21a2b186b56e9608329bef4b149e041806da318d
Alessio-Siciliano <alessiosiciliano@outlook.it>
2024-11-26 16:38:03 +0100

    Add more functions to Bigquery module

---
+++ bigquery/Dataset.py
@@ -0,0 +1,20 @@
+class Dataset:
+    access_entries          = None
+    created_on               = None
+    dataset_id              = None
+    description             = None
+    friendly_name           = None
+    full_dataset_id         = None
+    is_case_insensitive     = None
+    labels                  = None
+    location                = None
+    max_time_travel_hours   = None
+    modified                = None
+    project                 = None
+    
+    def set_attribute(self, attribute_name: str, attribute_value):
+        if getattr(self, attribute_name) is not None:
+            setattr(self, attribute_name, attribute_value)
+    
+    def get_attribute(self, attribute_name: str):
+        return getattr(self, attribute_name)
--- utils/Exceptions.py
+++ utils/Exceptions.py
@@ -2,4 +2,6 @@ class InvalidArgumentToFunction(Exception):
     def __init__(self) -> None:
         message = 'Please pass the correct parameters to the function!'
         Exception.__init__(self, message)
-    
+
+class ErrorDuringConnection(Exception):
+    pass
